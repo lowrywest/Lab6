d7773b560145da2f654624c951ebd30d00324d8b
Lowry West <4lowrywest@gmail.com>
2019-02-21 14:58:23 -0600

    Rectangle class

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,29 @@
 
-public class Rectangle {
+public class Rectangle extends Polygon
+{
+	public double height;
+	public double width;
+
+	public Rectangle(String id, double width, double height) 
+	{
+		super(id);
+		this.height=height;
+		this.width=width;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getArea()
+	{
+		// TODO Auto-generated method stub
+		return width*height;
+	}
+
+	@Override
+	public String getShapeType() 
+	{
+		// TODO Auto-generated method stub
+		return "Rectangle";
+	}
 
 }
