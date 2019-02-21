96d27c1d7964a0c3d8d207e4c740a58785b341f3
Lowry West <4lowrywest@gmail.com>
2019-02-21 11:15:50 -0600

    fix areacompare method

--- ShapeAreaComparator.java
+++ ShapeAreaComparator.java
@@ -32,8 +32,18 @@ public class ShapeAreaComparator implements Comparator<Shape>
 	@Override
 	public int compare(Shape s1, Shape s2)
 	{
-		int val= s1.compareTo(s2);
-		return val;
+		if(s1.getArea()<s2.getArea())
+		{
+			return -1;
+		}
+		else if(s1.getArea()>s2.getArea())
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
 		// TODO: complete this...
 	}
 
