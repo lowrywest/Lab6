6f4e723ce32c24a239e9e9933414f3029715772c
Lowry West <4lowrywest@gmail.com>
2019-02-23 17:52:08 -0600

    square class

--- Square.java
+++ Square.java
@@ -1,4 +1,16 @@
 
-public class Square {
+public class Square extends Rectangle
+{
+	public Square(String id, double size) 
+	{
+		super(id, size/2, size/2);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getShapeType()
+	{
+		return "Square";
+		
+	}
 
 }
