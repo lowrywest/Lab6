3d6f62048e2cd9f03dc926206b9f30b78a2eedd1
lowrywest <47228110+lowrywest@users.noreply.github.com>
2019-02-26 17:41:05 -0600

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,14 +1,18 @@
 
 public class Rectangle extends Polygon
 {
+	//global variables used throughout the class
 	public double height;
 	public double width;
 
 	public Rectangle(String id, double width, double height) 
 	{
+		//calling the super constructor
 		super(id);
+		//setting the variables
 		this.height=height;
 		this.width=width;
+		//putting the lengths in the ArrayList
 		sideLengths.add(width);
 		sideLengths.add(width);
 		sideLengths.add(height);
@@ -19,6 +23,7 @@ public class Rectangle extends Polygon
 	@Override
 	public double getArea()
 	{
+		//calcuating the area
 		// TODO Auto-generated method stub
 		return width*height;
 	}
