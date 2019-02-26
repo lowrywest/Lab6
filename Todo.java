1bd79f911810025254dfb70c15566124714edb9c
lowrywest <47228110+lowrywest@users.noreply.github.com>
2019-02-26 17:31:43 -0600

    Create Todo.java

---
+++ Todo.java
@@ -0,0 +1,45 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-26-19
+OU ID_______________: 113410037
+OU EMAIL____________: lowry.y.west-1@ou.edu
+
+Tasks:
+1. Rectangle Class Create and Finish
+2. Square Class Create and Finish
+3. Shape Class Finish
+4. Polygon Class Finish
+5. Trapezoid Class Finish
+6. Equilateral Triangle Class Finish
+7. Shape Sorter Class Finish
+8. Shape Area Comparator Class Finish
+9. Shape Perimeter Comparator Class Finish
+10. Shape Sorter Test Finish
+11. Shape Test Finish
+
+Estimated Time (minutes):
+1. 10
+2. 5
+3. 20
+4. 5
+5. 10
+6. 10
+7. 25
+8. 10
+9. 5
+10. 25
+11. 25
+
+Actual Time (minutes):
+1. 15
+2. 5
+3. 25
+4. 15
+5. 15
+6. 20
+7. 25
+8. 15
+9. 5
+10. 15
+11. 25
+*/}
