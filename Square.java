79aa65eeed80c0823a0e42e60375914442a5d609
lowrywest <47228110+lowrywest@users.noreply.github.com>
2019-02-26 20:12:30 -0600

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,12 +1,12 @@
 
 public class Square extends Rectangle
 {
-	public double size;
+	
 	
 	public Square(String id,double size)
 	{
 		super(id ,size, size);
-		this.size=size;
+		
 		// TODO Auto-generated constructor stub
 	}
 	
